e002a0c640e5a5a6325f1687da31e1cc0fe1eddd
Fu11Stack <96635777+Fu11Stack@users.noreply.github.com>
2024-04-16 15:30:23 -0700

    Add files via upload

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+
+//user model
+
+const userSchema = new mongoose.Schema({
+  username: {
+   type: String,
+   required: true
+  },
+  password: {
+   type: String,
+   required: true
+  },
+  roles: [{
+   type: String,
+   defualt: "Employee"
+  }],
+  active: {
+   type: Boolean,
+   default: true
+  }
+})
+
+
+module.exports = mongoose.model('User',userSchema)
